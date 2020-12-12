49bd255d42f5a915c0f6c0d6f7626002ea1e957d
flaaaps <julianwening4@gmail.com>
2020-12-12 20:20:39 +0100

    :rocket: Convert to functional component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import cx from 'classnames';
 
 import coronaImage from './images/image.png';
-class App extends React.Component {
-    state = {
-        data: {},
-        country: '',
+
+const App = () => {
+    const [data, setData] = useState({});
+    const [country, setCountry] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    const handleCountryChange = async (newCountry) => {
+        setLoading(true);
+        console.log(newCountry, 'COUUUUUUUUUUUUNTRY');
+        const fetchedData = await fetchData(newCountry);
+        console.log(fetchedData, 'FETCHED DATA');
+        setCountry(newCountry);
+        console.log(newCountry, 'NEW COUNTRY');
+        setData(fetchedData);
+        console.log(country, 'SET COUNTRY', data);
+        setLoading(false);
     };
 
-    handleCountryChange = async (country) => {
-        const fetchedData = await fetchData(country);
-        console.log(fetchedData);
-
-        this.setState({ data: fetchedData, country: country });
-    };
-
-    async componentDidMount() {
-        const fetchedData = await fetchData();
-        this.setState({ data: fetchedData });
-    }
-
-    render() {
-        const { data, country } = this.state;
-        return (
-            <div className={styles.container}>
-                <img className={styles.image} src={coronaImage} alt="COVID-19" />
-                <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Cards data={data} />
-                <Chart data={data} country={country} />
+    useEffect(() => {
+        const fetchAPI = async () => {
+            const fetchedData = await fetchData();
+            setData(fetchedData);
+            setLoading(false);
+        };
+        fetchAPI();
+    }, []);
+    return (
+        <div className={styles.container}>
+            <div className={cx(styles.loadingOverlay, loading ? styles.loading : null)}>
+                <i className="fas fa-spinner"></i>
+                <p>Loading...</p>
             </div>
-        );
-    }
-}
+            <img className={styles.image} src={coronaImage} alt="COVID-19" />
+            <CountryPicker handleCountryChange={handleCountryChange} />
+            <Cards data={data} />
+            <Chart data={data} country={country} />
+        </div>
+    );
+};
 
 export default App;
